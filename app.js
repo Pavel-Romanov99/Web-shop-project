ef03aa44e216557b76cf57c44bc4891c3ee6c3d3
Pavel Romanov <pavelromanov@Pavels-MacBook-Pro.local>
2021-12-25 22:32:57 +0200

    make it better

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 const path = require("path");
 const mongoose = require("mongoose");
-const bcrypt = require("bcrypt");
+const bcrypt = require("bcryptjs");
 var fs = require("fs");
 //in order to check if someone is logged in we use sessions
 const session = require("express-session");
